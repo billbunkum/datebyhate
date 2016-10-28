bb22e20a3fda3d0ef8bd7bd8cf86ddc3c6fd654b
billbunkum <william.richard.hurst@gmail.com>
2016-10-28 12:19:44 -0400

    play with authentication; need wire up for accounts/profile/api/films

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^$', login_required(views.IndexView.as_view()), name='index'),
 ]
--- interest/serializers.py
+++ interest/serializers.py
@@ -26,16 +26,23 @@ class FilmSerializer(serializers.ModelSerializer):
 #        read_only = ('current_user', )
 
 class InterestSerializer(serializers.ModelSerializer):
-    #custom method field
-    current_user = serializers.SerializerMethodField('_user')
+    #custom method field; serializerMethodFields are read_only and get their values by calling a method, in this case,  '_user'
+    #'()' defaults to get_<field_name>, i.e. get_current_user, which is the def below
+    current_user = serializers.SerializerMethodField()
+    current_user_id = serializers.SerializerMethodField()
 
     #using custom field '_user' to create a method
-    def _user(self, obj):
+    def get_current_user(self, obj):
         user = self.context['request'].user
         return user
+
+    def get_current_user_id(self, obj):
+#        user_id = self.context['request'].user_id
+#        user = self.context['request'].user
+        return User.objects.get(pk=obj.id)
 
     #adding 'current_user' w/ request.user as 'user' to fields
     class Meta:
         model = Interest
-        fields = ( 'id', 'date', 'current_user', 'film', )
-        read_only = ( 'id', 'current_user', )
+        fields = ( 'id', 'date', 'current_user', 'current_user_id', 'film', )
+        read_only = ( 'id', 'current_user_id', )
