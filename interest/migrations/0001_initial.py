c1201acdd4b9e0ad48cdc54565bd723601b0fa63
billbunkum <william.richard.hurst@gmail.com>
2016-11-16 14:11:18 -0500

    delete migrations for re-deploy

--- interest/migrations/0001_initial.py
+++ interest/migrations/0001_initial.py
@@ -15,18 +15,20 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Film',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=300)),
                 ('genre', models.CharField(max_length=300)),
                 ('director', models.CharField(blank=True, max_length=300)),
+                ('imdbID', models.CharField(max_length=300, default='empty')),
+                ('plot', models.CharField(blank=True, max_length=1000)),
+                ('url', models.URLField(blank=True, default='http://')),
             ],
         ),
         migrations.CreateModel(
             name='Interest',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
                 ('date', models.DateTimeField(auto_now_add=True)),
-                ('has_hate', models.BooleanField(default=False)),
                 ('film', models.ForeignKey(to='interest.Film')),
                 ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
