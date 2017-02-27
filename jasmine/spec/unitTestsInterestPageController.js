96a388a1feeadd1cd4f2b61595f4601ed4531ede
billbunkum <william.richard.hurst@gmail.com>
2017-02-27 18:28:33 -0500

    try first test on interestPageController.searchFilms()

---
+++ jasmine/spec/unitTestsInterestPageController.js
@@ -0,0 +1,7 @@
+// describe("searchFilms", function () {
+//     it("returns error message if data.Response === 'False'", function() {
+//         expect(interestPageController.searchFilms()).toEqual()
+//     }
+// })
+
+    // interestPageController.searchFilms(), if data.Response === False
