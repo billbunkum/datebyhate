cc87907d311df0c5734d7289dc16004db5290b41
billbunkum <william.richard.hurst@gmail.com>
2017-03-03 21:42:56 -0500

    wire up accounts' URLs w/views; still unsure about HMAC workflow views

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,13 +6,13 @@ from . import views
 # registration.backends.hmac.urls sets up the views from 'django.contrib.auth (login, logout, password reset, etc.)'
 
 urlpatterns = [
-    # url('^', include('django.contrib.auth.urls')),
     url(r'^', include('registration.backends.hmac.urls')),
-    # url(r'^register/$', views.register, name="register"),
     url(r'^activate/$', views.activate, name="activate"),
+    url(r'^activation-complete/$', views.activation_complete, name="activation_complete"),
+    url(r'^activation-email-subject/$', views.activation_email_subject, name="activation_email_subject"),
+    url(r'activation-email/$', views.activation_email, name="activation_email"),
     url(r'^register/$', views.register, name="register"),
-    # url(r'^login/$', views.login, name="login"),
-    # url(r'^logout/$', views.logout, name="logout"),
+    url(r'^registration-complete/$', views.registration_complete, name="registration_complete"),
     url(r'^login/$', 'django.contrib.auth.views.login', { 'authentication_form': LoginForm }, name="login"),
     url(r'^logout/$', 'django.contrib.auth.views.logout', { 'template_name': 'registration/logout.html' }, name="logout"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.core.mail import send_mail
+from django.contrib.sites.requests import RequestSite
+from django.contrib.sites.shortcuts import get_current_site
+from django.contrib.auth.models import User
+
 #send_mail(subject, message, from_email, recipient_list, fail_silently=False, auth_user=None, auth_password=None, connection=None, html_message=None
 
 # from .forms import UserRegistrationForm
@@ -10,45 +14,66 @@ from registration.backends.hmac.views import RegistrationView
 
 # from django.template import RequestContext
 
-
-def registration()
-        # #need to send activation e-mail here
-        # messages.success(request, 'Check your e-mail to activate your account.')
-    pass
-
 #OLD register view
 def register(request):
     if request.method == "POST":
         # form = RegistrationForm(request.POST)
         form = UserRegistrationForm(request.POST)
-
         if form.is_valid():
             new_user = form.save(commit=False)
             #need to clean data before persisting to db
             new_user.set_password(form.cleaned_data["password"])
-
-
             new_user.save()
             messages.success(request, 'Congrats! You now exist.')
-
             # return redirect('accounts:activate')
             #heading to accounts.urls for 'login' endpoint
             return redirect('accounts:login')
-
     else:
         # form = RegistrationForm
         form = UserRegistrationForm
-
     context = {
         "form": form,
     }
-
     # return render(request, "registration/register_form.html", context)
     return render(request, "accounts/register.html", context)
 
+#informs user that an e-mail w/Activation info has been sent
+def registration_complete(request):
+    messages.success(request, 'Check your e-mail to activate your account.')
+    return render(request, "registration/registration_complete.html")
+
+#used if Activation fails
 def activate(request):
     context = {
         "activation_key": activation_key,
     }
     return render(request, "registration/activate.html", context)
 
+#notifies user that account is activated
+def activation_complete(request):
+    return render(request, "registration/activation_complete.html")
+
+#the subject line of the activation e-mail sent
+def activation_email_subject(request):
+    #*should* return either a RequestSite object
+    # user = User.objects.get(username=request.user.username)
+    user = User.objects.get(user=request.user)
+    site = get_current_site(request)
+    context = {
+        "activation_key": activation_key,
+        "expiration_days": expiration_days,
+        "user": user,
+        "site": site,
+    }
+    return render(request, "registration/activation_email_subjext.txt", context)
+
+def activation_email(request):
+    user = User.objects.get(user=request.user)
+    site = get_current_site(request)
+    context = {
+        "activation_key": activation_key,
+        "expiration_days": expiration_days,
+        "user": user,
+        "site": site,
+    }
+    return render(request, "registration/activation_email.txt", context)
