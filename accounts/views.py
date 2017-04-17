502c5bfe1687d97a0caa3f3cb07a59b968a3fb46
billbunkum <william.richard.hurst@gmail.com>
2017-04-17 12:32:54 -0400

    fuck django documentation; it can go straight to hell

--- accounts/views.py
+++ accounts/views.py
@@ -18,34 +18,30 @@ def registration(request):
         form = RegistrationForm(request.POST)
 
         if form.is_valid():
-            user = form.save(commit=False)
-            user.set_password(form.cleaned_data["password1"])
-            # user.save()
-            # pdb.set_trace()
+            #new_user is already validated form data
+            new_user = form.save(commit=False)
+            new_user.set_password(form.cleaned_data["password1"])
 
-            #generates activation key from user info on form
-            activation_key = regView.get_activation_key(user)
-            #returns a DICT of values to use as 'template context' for activation email
+            activation_key = get_activation_key(new_user)
 
-            context = email_context(activation_key)
-            # regView.get_email_context(activation_key)
-            # #setting email stuffz
-            # email_body_template = "registration/activation_email.txt"
-            # email_subject_template = "registration/activation_email_subject.txt"
+            #sets email_subject_template & email_body_template but I'm not sure I need to do this.
+            email_context = email_context()
 
-            #creates dummy user before activation; invokes send_activation_email(user)
-            user = regView.create_inactive_user(form)
-            regView.send_activation_email(user)
-            #Notifes of an e-mail being sent
-            return redirect('accounts:registration-complete')
+            regView.create_inactive_user(new_user)
+            #calls send_activation_email('user')
+
+            return render(request, "registration/registration_complete.html")
+
     else:
         form = RegistrationForm
     context = {
         "form": form,
     }
+
     return render(request, "registration/registration_form.html", context)
 
-def email_context(activation_key):
+# placeholder
+def email_context():
     email_subject_template = "registration/activation_email_subject.txt"
     email_body_template = "registration/activation_email.txt"
 
@@ -53,18 +49,27 @@ def email_context(activation_key):
         "email_subject_template": email_subject_template,
         "email_body_template": email_body_template
     }
+
     return context
 
 #informs user that an e-mail w/Activation info has been sent
 def registration_complete(request):
     return render(request, "registration/registration_complete.html")
 
-#used if Activation fails
+#used if Activation fails or succeeds
 def activate(request):
-    context = {
-        "activation_key": activation_key,
-    }
-    return render(request, "registration/activate.html", context)
+# currently an anonymous user...below makes little sense
+    user = request.user
+    actView = ActivationView()
+    username = actView.get_user(user.username)
+    activation_key = user.activation_key
+    if activation_key:
+        activation_complete()
+    else:
+        context = {
+            "activation_key": activation_key,
+        }
+        return render(request, "registration/activate.html", context)
 
 #notifies user that account is activated
 def activation_complete(request):
