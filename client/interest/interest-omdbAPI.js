857724c3fa0e704aadf2563f1bafc2a1f2fe2c42
billbunkum <billbunkum@billbunkums-MacBook-Air.local>
2018-01-05 03:51:46 +0800

    fix index.lock issue; append omdbapi key to requests

--- client/interest/interest-omdbAPI.js
+++ client/interest/interest-omdbAPI.js
@@ -5,7 +5,8 @@ function omdbAPI($resource) {
     // };
 
     // return omdb;
-    return $resource("https://www.omdbapi.com/?");
+    // return $resource("https://www.omdbapi.com/?");
+    return $resource("http://www.omdbapi.com/?i=tt3896198&apikey=8500307e/?");
 
 }
 
